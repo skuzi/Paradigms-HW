487647811b9f9ae3af60dbf468eb8cb1dd7fefed
skuzi <serg.kuzmin00@mail.ru>
2017-09-16 14:29:29 +0000

    Update list_task.py with pep8

--- list_task.py
+++ list_task.py
@@ -1,13 +1,7 @@
-# Remove equal adjacent elements
-#
-# Example input: [1, 2, 2, 3]
-# Example output: [1, 2, 3]
 def remove_adjacent(lst):
     return list(set(lst))
-# Merge two sorted lists in one sorted list in linear time
-#
-# Example input: [2, 4, 6], [1, 3, 5]
-# Example output: [1, 2, 3, 4, 5, 6]
+
+
 def linear_merge(lst1, lst2):
     i = 0
     j = 0
