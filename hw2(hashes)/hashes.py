bd8d4c63e0064f06f00130e39574c1a923bf7e7e
skuzi <serg.kuzmin00@mail.ru>
2017-09-23 18:41:36 +0000

    Update hashes.py

--- hw2(hashes)/hashes.py
+++ hw2(hashes)/hashes.py
@@ -9,7 +9,7 @@ hasher = hasherr()
 def go(directory):
     for d, dirs, files in os.walk(directory):
         for f in files:
-            if f[0] != '~' and f[0] != '.':
+            if f[0] != '~' and f[0] != '.' and not os.path.islink(f_pname):
                 pathes.append(os.path.join(d, f))
         for dirr in dirs:
             go(dirr)
@@ -24,22 +24,22 @@ def get_hash(filename):
             hasher.update(data)
     return hasher.digest()
 
-
-params = sys.argv
-if len(params) > 1:
-    directory = params[1]
-go(directory)
-hashes = {}
-for path in pathes:
-    h = get_hash(path)
-    if h not in hashes:
-        hashes[h] = [1, [path]]
-    else:
-        hashes[h][1].append(path)
-        hashes[h][0] += 1
-    hasher = hasherr()
-for cnt, files in hashes.values():
-    if cnt > 1:
-        print(':'.join(files))
+def main():
+    params = sys.argv
+    if len(params) > 1:
+        directory = params[1]
+    go(directory)
+    hashes = {}
+    for path in pathes:
+        h = get_hash(path)
+        if h not in hashes:
+            hashes[h] = [1, [path]]
+        else:
+            hashes[h][1].append(path)
+            hashes[h][0] += 1
+        hasher = hasherr()
+    for cnt, files in hashes.values():
+        if cnt > 1:
+            print(':'.join(files))
 if __name__ == "__main__":
     main()
