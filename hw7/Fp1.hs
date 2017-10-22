c766cc77b8acd24ef88fe0ae20758cbdead5c86f
skuzi <serg.kuzmin00@mail.ru>
2017-10-22 16:20:11 +0000

    Update Fp1.hs

--- hw7/Fp1.hs
+++ hw7/Fp1.hs
@@ -6,32 +6,22 @@ tail' (_:xs) = xs
 --3. Take
 take' 0 xs = []
 take' n [] = []
-take' n (x:xs) = x:take (n-1) xs
+take' n (x:xs) = x:take' (n-1) xs
 --4. Drop
 drop' 0 xs = xs
 drop' _ [] = []
-drop' n (x:xs) = drop (n-1) xs
+drop' n (x:xs) = drop' (n-1) xs
 --5. Filter
 filter' f [] = []
-filter' f (x : xs) = if(f(x)) then x:filter f xs else filter f xs
+filter' f (x : xs) = if(f(x)) then x:filter' f xs else filter' f xs
 --6. Foldl
 foldl' _ z [] = z
-foldl' f z (x : xs) = foldl f (f z x) xs
+foldl' f z (x : xs) = foldl' f (f z x) xs
 --7. Concat
 concat' [] ys = ys
 concat' (x:xs) ys = x:concat' xs ys
 --8. QuickSort
-smaller' _ [] = []
-smaller' key (x:xs) = if(x <= key) then x:smaller' key xs else smaller' key xs
-greater' _ [] = []
-greater' key (x:xs) = if(x > key) then x:greater' key xs else greater' key xs
 quicksort' [] = []
-quicksort' (x:xs) = (quicksort' (smaller' x xs)) `concat'` (x:(quicksort' (greater' x xs)))
---8'. QuicSort v2
-quicksort'' [] = []
-quicksort'' (x:xs) = (quicksort'' (smaller'' x xs)) `concat'` (x:(quicksort'' (greater'' x xs)))
-					where 
-						smaller'' _ [] = []
-						smaller'' key (x:xs) = if(x <= key) then x:smaller'' key xs else smaller'' key xs
-						greater'' _ [] = []
-						greater'' key (x:xs) = if(x > key) then x:greater'' key xs else greater'' key xs
+quicksort' (x:xs) = (quicksort'(filter' (less x) xs)) `concat'` (x:quicksort'(filter' (great x) xs)) where
+                                                                                                            less x y = if(y <= x) then True else False
+                                                                                                            great x y = if(y > x) then True else False
