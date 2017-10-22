a94064e82ba26c7d97d92544372fedc04c23a3b7
skuzi <serg.kuzmin00@mail.ru>
2017-10-22 16:30:58 +0000

    Update Fp1.hs

--- hw7/Fp1.hs
+++ hw7/Fp1.hs
@@ -12,7 +12,7 @@ drop' 0 xs = xs
 drop' _ [] = []
 drop' n (x:xs) = drop' (n-1) xs
 --5. Filter
-filter' f [] = []
+filter' _ [] = []
 filter' f (x : xs) = if(f(x)) then x:filter' f xs else filter' f xs
 --6. Foldl
 foldl' _ z [] = z
@@ -22,6 +22,4 @@ concat' [] ys = ys
 concat' (x:xs) ys = x:concat' xs ys
 --8. QuickSort
 quicksort' [] = []
-quicksort' (x:xs) = (quicksort'(filter' (less x) xs)) `concat'` (x:quicksort'(filter' (great x) xs)) where
-                                                                                                            less x y = if(y <= x) then True else False
-                                                                                                            great x y = if(y > x) then True else False
+quicksort' (x:xs) = (quicksort' (filter' (<=x) xs)) `concat'` (x:quicksort' (filter' (>x) xs))
