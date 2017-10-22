ac86281e0b076ff54cd5c5d155a426923781b3b7
skuzi <serg.kuzmin00@mail.ru>
2017-10-22 16:00:30 +0000

    Update Fp1.hs

--- hw7/Fp1.hs
+++ hw7/Fp1.hs
@@ -1,27 +1,29 @@
---useful operator
-(++) [] ys = ys
-(++) (x : xs) ys = x : (xs ++ ys)
 --1. Head
-head (x : xs) = x
+head' (x:_) = x
 --2. Tail
-tail []  = []
-tail (x : xs) = xs
+tail' []  = []
+tail' (_:xs) = xs
 --3. Take
-take 0 xs = []
-take n [] = []
-take n (x : xs) = [x] ++ take (n-1) xs
+take' 0 xs = []
+take' n [] = []
+take' n (x:xs) = x:take (n-1) xs
 --4. Drop
-drop 0 xs = xs
-drop n [] = []
-drop n (x : xs) = drop (n-1) xs
+drop' 0 xs = xs
+drop' _ [] = []
+drop' n (x:xs) = drop (n-1) xs
 --5. Filter
-filter f [] = []
-filter f (x : xs) = if(f(x)) then [x] ++ filter f xs else filter f xs
+filter' f [] = []
+filter' f (x : xs) = if(f(x)) then x:filter f xs else filter f xs
 --6. Foldl
-foldl _ z [] = z
-foldl f z (x : xs) = foldl f (f z x) xs
+foldl' _ z [] = z
+foldl' f z (x : xs) = foldl f (f z x) xs
 --7. Concat
-concat xs ys = xs ++ ys
+concat' [] ys = ys
+concat' (x:xs) ys = x:concat' xs ys
 --8. QuickSort
-quicksort [] = []
-quicksort (x:xs) = quicksort [a | a<-xs, a <= x] ++ [x] ++ quicksort [a | a<-xs, a > x]
+smaller' _ [] = []
+smaller' key (x:xs) = if(x <= key) then x:smaller' key xs else smaller' key xs
+greater' _ [] = []
+greater' key (x:xs) = if(x > key) then x:greater' key xs else greater' key xs
+quicksort' [] = []
+quicksort' (x:xs) = (quicksort' (smaller' x xs)) `concat'` (x:(quicksort' (greater' x xs)))
