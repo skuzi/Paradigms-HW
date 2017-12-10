aaf2e584e277fdd1322477b0caa52c9af8090597
skuzi <serg.kuzmin00@mail.ru>
2017-12-10 22:33:14 +0300

    update main.cpp

--- hw9/src/main.cpp
+++ hw9/src/main.cpp
@@ -2,14 +2,14 @@
 #include <cstdlib>
 #include <ctime>
 #include <utility>
+#include <list>
 #include <algorithm>
 #include <iostream>
 #include <atomic>
 
 std::size_t MAX_DEPTH;
 
-Task sort_tasks[1000000];
-std::size_t last_task;
+std::list<Task> sort_tasks;
 
 int check(std::vector<int> &a) {
     for(std::size_t i = 0; i < a.size() - 1; i++) {
@@ -42,68 +42,36 @@ struct Query {
     ~Query(){}
 };
 
-Query left[1000000];
-std::size_t last_left;
-Query right[1000000];
-std::size_t last_right;
+std::list<Query> left, right;
 
+
+pthread_mutex_t mut;
 void qsort(void* q) {
     Query* s = (Query*) q;
-    if(s->l > s->r)
+    if(s->l >= s->r)
         return;
-
     if(s->depth == MAX_DEPTH) {
         std::sort(a.begin() + s->l, a.begin() + s->r + 1);
         return;
     }
-
-    pthread_mutex_t mut1;
-    pthread_mutex_init(&mut1, NULL);
-    pthread_mutex_lock(&mut1);
-    std::size_t i = s->l, j = s->r;
-    //std::cout << i << ' ' << j << '\n';
-    int pivot = a[(i + j + 1) / 2];
-    //std::cout << "pivot is: " << pivot << '\n';
-    while(i <= j) {
-        while(a[i] < pivot) {
-            //std::cout << "i is: " << i << '\n';
-            ++i;
-        }
-        while(a[j] > pivot){
-            //std::cout << "j is: " << j << '\n';
-            --j;
-        }
-        if(i <= j) {
-            /*puts("before: ");
-            for(int t = 0; t < a.size(); t++)
-                std::cout << a[t] << ' ';*/
-            //puts("\n");
-            std::swap(a[i], a[j]);
-            ++i;
-            --j;
-            /*puts("after: ");
-            for(int t = 0; t < a.size(); t++)
-                std::cout << a[t] << ' ';
-            std::cout << '\n';*/
-        }
-    }
-    pthread_mutex_unlock(&mut1);
-    pthread_mutex_destroy(&mut1);
-    std::size_t m = (s->l + s->r + 1) / 2;
-
-    pthread_mutex_t mut;
-    pthread_mutex_init(&mut, NULL);
     pthread_mutex_lock(&mut);
-    left[last_left++] = Query(s->th_pool, s->l, j, s->depth + 1);
-    right[last_right++] = Query(s->th_pool, i, s->r, s->depth + 1);
 
-    sort_tasks[last_task++] = Task(qsort, &left[last_left - 1]);
-    sort_tasks[last_task++] = Task(qsort, &right[last_right - 1]);
+    int pivot = a[(s->l + s->r + 1) / 2];
+    auto it1 = std::partition(a.begin() + s->l, a.begin() + s->r + 1, [pivot](int i){ return i < pivot; });
+    auto it2 = std::partition(it1, a.begin() + s->r + 1, [pivot](int i){ return i <= pivot; });
+    
+    left.push_back(Query(s->th_pool, s->l, it1 - a.begin() - 1, s->depth + 1));
+    right.push_back(Query(s->th_pool, it2 - a.begin(), s->r, s->depth + 1));
+    sort_tasks.push_back(Task(qsort, &left.back()));
+    Task& last1 = sort_tasks.back();
+    sort_tasks.push_back(Task(qsort, &right.back()));
+    Task& last2 = sort_tasks.back();
+
     pthread_mutex_unlock(&mut);
-    pthread_mutex_destroy(&mut);
 
-    s->th_pool->submit(&sort_tasks[last_task - 1]);
-    s->th_pool->submit(&sort_tasks[last_task - 2]);
+    s->th_pool->submit(&last1);
+    s->th_pool->submit(&last2);
+
 }
 
 void do_tasks(std::size_t thread_cnt) {
@@ -112,13 +80,19 @@ void do_tasks(std::size_t thread_cnt) {
 
     Query s(&pool, 0, a.size() - 1);
 
-    sort_tasks[last_task++] = Task(qsort, &s);
+    sort_tasks.push_back(Task(qsort, &s));
     
-    pool.submit(sort_tasks);
+    pool.submit(&sort_tasks.back());
 
-    for(std::size_t i = 0; i < last_task; i++) {
-        sort_tasks[i].wait();
+    while(!sort_tasks.empty()) {
+        sort_tasks.front().wait();
+        sort_tasks.pop_front();
     }
+    
+    if(check(a))
+        puts("FAIL");
+    else
+        puts("SUCCESS");
     std::time_t end_time = time(NULL);
     std::cout << (end_time - start_time) << '\n';
 }
@@ -126,21 +100,20 @@ void do_tasks(std::size_t thread_cnt) {
 int main(int argc, char* argv[]) {
     if(argc != 4)
         return -1;
+    
     srand(42);
     std::size_t size = atoi(argv[2]);
     MAX_DEPTH = atoi(argv[3]);
+
     a.resize(size);
     for(std::size_t i = 0; i < size; i++) {
         a[i] = rand();
     }
 
+    pthread_mutex_init(&mut, NULL);
 
     do_tasks(atoi(argv[1]));
-    if(check(a)) {
-        puts("FAIL");
-        return 1;
-    }
 
-    puts("SUCCESS");
+    pthread_mutex_destroy(&mut);    
     return 0;
 }
